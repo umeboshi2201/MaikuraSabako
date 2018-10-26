425feaf2b8c54901341c33794d0918cbd027995b
umeboshi2201 <umeume2201@gmail.com>
2018-10-26 09:52:34 +0900

    Make handling filename vars better

--- main.py
+++ main.py
@@ -24,20 +24,25 @@ async def on_ready():
         elif(sys.argv[1] == 'end'): 
             end_message = character.getEndMessage()
 
+            await client.send_message(channel, end_message)
+
             if(len(sys.argv) >= 3):
+
                 log_dir_name = 'Logs'
                 new_filename = 'ServerLog_' + datetime.datetime.today().strftime("%Y%m%d%H%M%S") + '.txt'
                 latest_filename = 'LatestLog.txt'
                 path_new_filename = os.path.join(log_dir_name, new_filename)
                 path_latest_filename = os.path.join(log_dir_name, latest_filename)
                 os.makedirs(log_dir_name, exist_ok=True)
+
                 with open(path_new_filename, 'w') as f, open(path_latest_filename, 'w') as lf:
                     f.write(sys.argv[2])
                     lf.write(sys.argv[2])
-                end_message = end_message + '\n' + character.getLogMessage(sys.argv[2]);
+                log_message = character.getLogMessage(sys.argv[2]);
 
-            await client.send_message(channel, end_message)
-            await client.send_file(channel, path_new_filename)
+                await client.send_message(channel, log_message)
+                await client.send_file(channel, path_new_filename)
+
 
             print(end_message)
         else:
